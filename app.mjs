e76fb41fd9cfd1cbba9af0c6198833796e39607b
Wooper1000 <woper1000@gmail.com>
2022-03-21 20:53:49 +0300

    'logger'

--- app.mjs
+++ app.mjs
@@ -2,22 +2,40 @@ import axios from "axios";
 import * as fs from 'fs';
 import express from 'express';
 import tf, {linalg, log} from '@tensorflow/tfjs-node';
-import * as canvas from 'canvas';
+//import * as canvas from 'canvas';
 import * as faceapi from '@vladmandic/face-api';
 import dateFormat, {masks} from "dateformat";
+import pino from 'pino'
+import expressPino from 'express-pino-logger'
+
+const logger = pino(
+    {
+        level: process.env.LOG_LEVEL || 'debug',
+        prettyPrint: {
+            colorize: true,
+            levelFirst: true,
+            translateTime: "yyyy-dd-mm, h:MM:ss TT",
+        },
+    },
+    pino.destination("./pino-logger.log")
+);
+const expressLogger = expressPino({logger})
+const app = express()
+
+const PORT = 3000
 
 
 await faceapi.nets.ssdMobilenetv1.loadFromDisk('./models')
 await faceapi.nets.faceLandmark68Net.loadFromDisk('./models')
 await faceapi.nets.faceRecognitionNet.loadFromDisk('./models')
-const {Canvas, Image, ImageData} = canvas
-faceapi.env.monkeyPatch({Canvas, Image, ImageData})
+//const {Canvas, Image, ImageData} = canvas
+//faceapi.env.monkeyPatch({ Image, ImageData})
 
-const app = express()
-const PORT = 3000
+
 app.listen(PORT, () => {
-    console.log('App is listening on port', PORT)
+    logger.info(`App is listening on port` + PORT)
 })
+app.use(expressLogger)
 
 
 const instance = axios.create({
@@ -31,6 +49,7 @@ const instance = axios.create({
 
 async function image(file) {
     const decoded = tf.node.decodeImage(file);
+    //const decoded = tf.node.decodeJpeg(file)
     const casted = decoded.toFloat();
     const result = casted.expandDims(0);
     decoded.dispose();
@@ -77,43 +96,83 @@ const faceMatcher = new faceapi.FaceMatcher(controlDescriptors)
 
 let Door = new class {
     isOpen = false
+
     openDoor() {
-        instance.post('places/5626227/accesscontrols/63309/actions', {"name": "accessControlOpen"}).then(res=>{
-            if(res.status===200) {
-                console.log('Дверь открыта')
+        instance.post('places/5626227/accesscontrols/63309/actions', {"name": "accessControlOpen"}).then(res => {
+            if (res.status === 200) {
+                logger.info('Дверь открыта')
             }
         })
     }
-    closeDoor(){
+
+    closeDoor() {
         this.isOpen = false
     }
 }
 
+// const openDoorByFace = () => {
+//     instance.get('places/5626227/accesscontrols/63309/videosnapshots').then(async res => {
+//         console.log(res.status)
+//     //     let photoName = dateFormat(new Date(), 'HH-MM-ss_dd-mm-yyyy')
+//         //let stream = fs.createWriteStream(`./visitors/${photoName}.jpeg`)
+//         let stream = []
+//          res.data.pipe(stream)
+//         stream.on('finish', async () => {
+//             let visitor = await getDescriptors(await image(fs.readFileSync(stream.path)),`visitor_`+photoName)
+//             //let visitor = await getDescriptors(await image(fs.readFileSync('./test1.jpeg')), `visitor_` + photoName)
+//             if (visitor) {
+//                 const bestMatch = faceMatcher.findBestMatch(visitor[0].descriptors)
+//                 console.log(bestMatch.toString().split(' ')[0])
+//                 if (bestMatch.toString().split(' ')[0] !== 'unknown' && !Door.isOpen) {
+//                     Door.openDoor()
+//                     Door.isOpen = true
+//                     setTimeout(Door.closeDoor.bind(Door),5000)
+//                     fs.rename(stream.path, stream.path + '_' + bestMatch._label + '.jpeg', () => {
+//                     })
+//                 }
+//                 else if(bestMatch.toString().split(' ')[0] === 'unknown'){
+//                     console.log('zahel')
+//                     fs.unlink(stream.path, () => {})
+//                 }
+//             }
+//             else {
+//                 fs.unlink(stream.path, () => {})
+//             }
+//         })
+//      })
+//  }
 const openDoorByFace = () => {
-    instance.get('places/5626227/accesscontrols/63309/videosnapshots').then(async res => {
-        console.log(res.status)
-        let photoName = dateFormat(new Date(), 'HH-MM-ss_dd-mm-yyyy')
-        let stream = fs.createWriteStream(`./visitors/${photoName}`)
-        res.data.pipe(stream)
-        stream.on('finish', async () => {
-            let visitor = await getDescriptors(await image(fs.readFileSync(stream.path)),`visitor_`+photoName)
-            //let visitor = await getDescriptors(await image(fs.readFileSync('./test1.jpeg')), `visitor_` + photoName)
-            if (visitor) {
-                const bestMatch = faceMatcher.findBestMatch(visitor[0].descriptors)
-                console.log(bestMatch.toString().split(' ')[0])
-                if (bestMatch.toString().split(' ')[0] !== 'unknown' && !Door.isOpen) {
-                    Door.openDoor()
-                    Door.isOpen = true
-                    setTimeout(Door.closeDoor.bind(Door),5000)
-                    fs.rename(stream.path, stream.path + '_' + bestMatch._label + '.jpeg', () => {
+    try {
+        instance.get('places/5626227/accesscontrols/63309/videosnapshots').then(async res => {
+            let photoName = dateFormat(new Date(), 'HH-MM-ss_dd-mm-yyyy')
+            let stream = fs.createWriteStream(`./visitors/${photoName}.jpeg`)
+            res.data.pipe(stream)
+            stream.on('finish', async () => {
+                let visitor = await getDescriptors(await image(fs.readFileSync(stream.path)), `visitor_` + photoName)
+                //let visitor = await getDescriptors(await image(fs.readFileSync('./test1.jpeg')), `visitor_` + photoName)
+                if (visitor) {
+                    const bestMatch = faceMatcher.findBestMatch(visitor[0].descriptors)
+                    logger.info(`${bestMatch.toString().split(' ')[0]}`)
+                    if (bestMatch.toString().split(' ')[0] !== 'unknown' && !Door.isOpen) {
+                        Door.openDoor()
+                        Door.isOpen = true
+                        setTimeout(Door.closeDoor.bind(Door), 5000)
+                        fs.rename(stream.path, stream.path + '_' + bestMatch._label + '.jpeg', () => {
+                        })
+                    } else if (bestMatch.toString().split(' ')[0] === 'unknown') {
+                        fs.unlink(stream.path, () => {
+                        })
+                    }
+                } else {
+                    fs.unlink(stream.path, () => {
                     })
                 }
-                else if(bestMatch.toString().split(' ')[0] === 'unknown'){
-                    fs.unlink(stream.path, () => {})
-                }
-            }
+            })
         })
-    })
+    }
+    catch(err){
+       logger.info(`${err}`)
+    }
 }
 
 setInterval(openDoorByFace, 1000)
